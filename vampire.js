ad22770870c240b3d1c61870759d92e1d81c94ab
nikolajjuuel <nikolaj.juuel@gmail.com>
2021-10-04 03:12:52 +0000

    added vampire challenges

--- vampire.js
+++ vampire.js
@@ -7,25 +7,42 @@ class Vampire {
   }
 
   /** Simple tree methods **/
-
+  
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numebrOfVampires = 0;
+    let currentVampire = this;
 
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numebrOfVampires++;
+    }
+
+    return numebrOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    const thisVampire = this.numberOfVampiresFromOriginal;
+    const thatVampire = vampire.numberOfVampiresFromOriginal;
 
+    if (thisVampire < thatVampire){
+      return true;
+    }
+    
+    return false;
   }
 
   /** Stretch **/
